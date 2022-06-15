ef82b96bb456693a0c8461dc4c825a75ade57d6e
Vasil Umanets <umvasya@gmail.com>
2022-06-15 12:56:14 +0300

    modal windows

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 var reglink = document.querySelector(".reg-link")
 var regpopup = document.querySelector(".reg-modal")
 
+var maplink = document.querySelector('.map-link')
+var mappopup = document.querySelector('.map-modal')
+
 reglink.addEventListener("click", function (evt) {
     evt.preventDefault();
     regpopup.classList.toggle('reg-modal-show')
+})
+
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (regpopup.classList.contains('reg-modal-show')) {
+            evt.preventDefault();
+            regpopup.classList.remove('reg-modal-show');
+        }             
+    }
+})
+
+
+maplink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log('mapppp')
+    mappopup.classList.toggle("map-modal-show")
+})
+
+window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+        if (mappopup.classList.contains('map-modal-show')) {
+            evt.preventDefault();
+            mappopup.classList.remove('map-modal-show');
+        }             
+    }
 })
